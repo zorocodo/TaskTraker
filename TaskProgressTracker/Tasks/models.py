c0d4106ac22a2a9fb5a238b78430912abd18e4a1
MKiranAchari <muralikiran534@gmail.com>
2026-02-19 17:55:33 +0530

    Created Project setup and a app for the database/api

---
+++ TaskProgressTracker/Tasks/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=150, blank=False)
+    description = models.CharField(blank=False, max_length=500)
+    target_min = models.IntegerField(default=1)
+    target_max = models.IntegerField(default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def latest_progress(self):
+        return self.progress_entries.order_by('-created_at').first()
+    
+    
+class ProgressEntry(models.Model):
+    task = models.ForeignKey(
+        Task, 
+        on_delete=models.CASCADE,
+        related_name='progress_entries'
+    )
+    percentage = models.PositiveSmallIntegerField()
+    note = models.CharField(blank=False, default='No Comments', max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    progress_value = models.IntegerField()
+
+    class Meta:
+        ordering = ['created_at']
